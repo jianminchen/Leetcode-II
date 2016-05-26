25020f112bbcae0d43bc3dde0ff9ee111496a1c0
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-05-25 23:17:55 -0400

    Create 96. Unique Binary Search Trees.py

---
+++ 96. Unique Binary Search Trees.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+            
+        T=[0 for i in xrange(n+1)]
+        T[0]=1
+        T[1]=1
+
+        for i in xrange(2,n+1):
+            for j in xrange(i):
+                T[i]+=T[j]*T[i-1-j]
+        return T[n]
