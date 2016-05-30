e7f04b2568cce006b223f892ded720b1bcdc430e
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-05-29 21:36:00 -0400

    Create 113. Path Sum II.py

---
+++ 113. Path Sum II.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, s):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        if root==None:
+            return []
+        self.res=[]
+        self.s=s
+        self.path(root, [root.val])
+        return self.res
+    
+    def path(self, root, pathlist):
+        if root.left==None and root.right==None:
+            if sum(pathlist)==self.s:
+                self.res.append(pathlist)
+            return
+        else:
+            if root.left!=None:
+                self.path(root.left, pathlist+[root.left.val])
+            if root.right!=None:
+                self.path(root.right, pathlist+[root.right.val])
