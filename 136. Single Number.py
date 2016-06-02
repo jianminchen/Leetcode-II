06092c1e8abaa0a5820c05dc9caf2e0d87f3a57c
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-02 18:38:13 -0400

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -7,8 +7,11 @@ class Solution(object):
         dic={}
         for i in nums:
             if i not in dic:
-                dic[i]=0
-            dic[i]+=1
+                dic[i]=1
+            elif dic[i]==2:
+                del dic[i]
+            else:
+                dic[i]+=1
         for i in dic:
             if dic[i]==1:
                 return i
