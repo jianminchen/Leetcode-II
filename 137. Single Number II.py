0a199a1e91b6d546ba39b9892ce49e358d99d81d
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-02 18:36:39 -0400

    Update 137. Single Number II.py

--- 137. Single Number II.py
+++ 137. Single Number II.py
@@ -7,8 +7,12 @@ class Solution(object):
         dic={}
         for i in nums:
             if i not in dic:
-                dic[i]=0
-            dic[i]+=1
+                dic[i]=1
+            elif dic[i]==3:
+                del dic[i]
+            else:
+                dic[i]+=1
         for i in dic:
             if dic[i]==1:
                 return i
+        
