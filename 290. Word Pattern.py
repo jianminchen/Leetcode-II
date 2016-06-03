a3fc43506a1567e76496996b8dd64dcdcfc0d422
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-03 11:44:12 -0400

    Create 290. Word Pattern.py

---
+++ 290. Word Pattern.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        pattern=list(pattern)
+        str=str.split(" ")
+        if len(pattern)!=len(str):
+            return False
+        dic={}
+        strlist={}
+        for i in xrange(len(pattern)):
+            if pattern[i] not in dic:
+                if str[i] not in strlist:
+                    dic[pattern[i]]=str[i]
+                    strlist[str[i]]=1
+                else:
+                    return False
+            else:
+                if dic[pattern[i]]!=str[i]:
+                    return False
+        return True
