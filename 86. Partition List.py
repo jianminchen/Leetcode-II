ba8fa3849526aaf941a8036fb90d38be4cba0a1b
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-04 13:10:03 -0400

    Create 86. Partition List.py

---
+++ 86. Partition List.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        dummy=ListNode(-1)
+        dummy.next=head
+        end_of_less_than_x=dummy
+        head_of_not_less_than_x=None
+        tail_of_not_less_than_x=None
+        while head:
+            if head.val<x:
+                if head_of_not_less_than_x:
+                    tail_of_not_less_than_x.next=head.next
+                    if tail_of_not_less_than_x:
+                        head.next=head_of_not_less_than_x
+                        end_of_less_than_x.next=head
+                        end_of_less_than_x=head
+                        head=tail_of_not_less_than_x.next
+                    else:
+                        end_of_less_than_x.next=head
+                        head.next=head_of_not_less_than_x
+                else:
+                    end_of_less_than_x=head
+                    head=head.next
+            else:
+                if head_of_not_less_than_x==None:
+                    head_of_not_less_than_x=head
+                tail_of_not_less_than_x=head
+                head=head.next
+
+        return dummy.next
