bbff9665ac4e563a4a210f252d09e9b6beaf809e
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-03 21:06:07 -0400

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         if x/10==0:
             return True
-        if x%10==0:
+        if x%10==0: #ex: 10, 100
             return False
         sum=0
         while x>0:
             sum=sum*10+x%10
-            if sum==x or sum==x/10:
+            if sum==x or sum==x/10: #depends on length of x is odd or even
                 return True
             x/=10
         return False
