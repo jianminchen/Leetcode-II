8635a171dc3fd8727f78bf7f52baa830222770f7
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-03 21:13:46 -0400

    Create 206. Reverse Linked List.py

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy=ListNode(-1)
+        nextnode=None
+        
+        while head:
+            temp=ListNode(head.val)
+            temp.next=nextnode
+            nextnode=temp
+            dummy.next=temp
+            head=head.next
+            
+        return dummy.next
