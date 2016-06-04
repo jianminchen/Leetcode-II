9e7f7316751c11d2831677aa5c5dcd7b16305f53
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-03 21:01:00 -0400

    Create 9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x/10==0:
+            return True
+        if x%10==0:
+            return False
+        sum=0
+        while x>0:
+            sum=sum*10+x%10
+            if sum==x or sum==x/10:
+                return True
+            x=x/10
+        return False
