62ed276572503b5788d07b78b7bcb8de9a271bad
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-05 16:06:20 -0400

    Create 89. Gray Code.py

---
+++ 89. Gray Code.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        initial=[0]
+        for i in xrange(n):
+            length=len(initial)
+            for j in xrange(length-1,-1,-1):
+                initial+=[initial[j]+2**i]
+        return initial
