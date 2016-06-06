c3cb0ccf34d25852d0ed4b46d1c2c850a3704383
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-06 11:21:04 -0400

    Rename 46. Permutations new.py to 46. Permutations new new.py

---
+++ 46. Permutations new new.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        firstnums=[]
+        firstnums[:]=nums[:]
+        while True:
+            ind1=-1
+            ind2=-1
+            for i in xrange(len(nums)-2,-1,-1):
+                if nums[i]<nums[i+1]:
+                    ind1=i
+                    break
+            if ind1<0:
+                nums[:]=nums[::-1]
+            else:
+                for i in xrange(ind1+1,len(nums)):
+                    if nums[i]>nums[ind1]:
+                        ind2=i
+                temp=nums[ind1]
+                nums[ind1]=nums[ind2]
+                nums[ind2]=temp
+                nums[:]=nums[:ind1+1]+nums[ind1+1:][::-1]
+
+            if nums!=firstnums:
+                res+=[nums[:]]
+            else:
+                res+=[nums[:]]
+                break
+        return res
