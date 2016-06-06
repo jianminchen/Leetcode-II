8bed9951521fd86aac74d84737ef838168b5f553
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-06 19:05:41 -0400

    Update 209. Minimum Size Subarray Sum.py

--- 209. Minimum Size Subarray Sum.py
+++ 209. Minimum Size Subarray Sum.py
@@ -11,12 +11,12 @@ class Solution(object):
         minn=sys.maxint
         if len(nums)==0:
             return 0
-        T=[sys.maxint for i in xrange(len(nums)+1)]
         for i in xrange(1,len(nums)+1):
+            if nums[i]>=s:
+                return 1
             for j in xrange(i+1):
                 if sum(nums[j:i+1])>=s:
-                    T[i]=i+1-j
-                    minn=min(minn,T[i])
+                    minn=min(minn,i+1-j)
         if minn==sys.maxint:
             return 0
         return minn
