65cdb0da27aad484ace98de08c19f70ececaaad2
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-06 19:02:34 -0400

    Create 209. Minimum Size Subarray Sum.py

---
+++ 209. Minimum Size Subarray Sum.py
@@ -0,0 +1,21 @@
+# timeout
+
+import sys
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        T=[sys.maxint for i in xrange(len(nums)+1)]
+        for i in xrange(1,len(nums)+1):
+            for j in xrange(i+1):
+                if sum(nums[j:i+1])>=s:
+                    T[i]=i+1-j
+        temp=min(T)
+        if temp==sys.maxint:
+            return 0
+        return min(T)
