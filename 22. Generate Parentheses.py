b053fc57e000d38d665599e5d85367a9c2df3839
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-09 11:07:18 -0400

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: List[str]
         """
         self.res=[]
-        numl, numr=0, 0
-        self.Parenthesis(numl, numr, "", n)
+        self.n=n
+        self.Parenthesis(0, 0, "")
         return self.res
         
-    def Parenthesis(self, numl, numr, path, n):
-        if len(path)==2*n:
+    def Parenthesis(self, numl, numr, path):
+        if len(path)==2*self.n:
             self.res+=[path[:]]
-        if numl<n:
-            self.Parenthesis(numl+1, numr, path+'(', n)
+        if numl<self.n:
+            self.Parenthesis(numl+1, numr, path+'(')
         if numr<numl:
-            self.Parenthesis(numl, numr+1, path+')', n)
+            self.Parenthesis(numl, numr+1, path+')')
