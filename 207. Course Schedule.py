f0226307940b41683bb87f2ee936fa8474874dc9
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-10 15:02:25 -0400

    Create 207. Course Schedule.py

---
+++ 207. Course Schedule.py
@@ -0,0 +1,29 @@
+# detect cycle in a undirected graph
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        visited=[False for i in xrange(numCourses)]
+        recstack=[False for i in xrange(numCourses)]
+        adj=[[] for i in xrange(numCourses)]
+        for i in prerequisites:
+            adj[i[1]]+=[i[0]]
+        for i in xrange(numCourses):
+            if self.helper(i, visited, recstack, adj):
+                return False
+        return True
+            
+    def helper(self, node, visited, recstack, adj):
+        if visited[node]==False:
+            visited[node]=True
+            recstack[node]=True
+            for i in adj[node]:
+                if visited[i]==False and self.helper(i, visited, recstack, adj):
+                    return True
+                elif recstack[i]:
+                    return True
+        recstack[node]=False
+        return False
